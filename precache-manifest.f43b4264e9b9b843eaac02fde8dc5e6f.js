6829779900a3885bccb6ffcb65b6242b5599cf71
Bomyung Choi <--help>
2021-01-23 19:36:12 +0900

    Updates

---
+++ precache-manifest.f43b4264e9b9b843eaac02fde8dc5e6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17b86d19ddb802c35e5af30b43c4e5c",
+    "url": "/staySafe-covid19/index.html"
+  },
+  {
+    "revision": "40dfb64501a0d5a40c97",
+    "url": "/staySafe-covid19/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "f9198ff4675fb57a1325",
+    "url": "/staySafe-covid19/static/css/main.b9b5f11d.chunk.css"
+  },
+  {
+    "revision": "40dfb64501a0d5a40c97",
+    "url": "/staySafe-covid19/static/js/2.e9687619.chunk.js"
+  },
+  {
+    "revision": "ae590546b09beec60f2626d9638fd897",
+    "url": "/staySafe-covid19/static/js/2.e9687619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9198ff4675fb57a1325",
+    "url": "/staySafe-covid19/static/js/main.068fbd18.chunk.js"
+  },
+  {
+    "revision": "84f195e34a081497c876",
+    "url": "/staySafe-covid19/static/js/runtime-main.b65e5ebf.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/staySafe-covid19/static/media/covid19.d7265326.png"
+  },
+  {
+    "revision": "123cde95531a9ac8aa0341c76adfe2f0",
+    "url": "/staySafe-covid19/static/media/map.123cde95.png"
+  }
+]);
